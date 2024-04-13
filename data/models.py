b91dd5f6c7cc0474519b80679f2732891de3c1c6
miray-mustafov <mustafovmiray@gmail.com>
2024-04-13 14:09:27 +0300

    forum db database

---
+++ data/models.py
@@ -0,0 +1,19 @@
+from datetime import date
+from pydantic import BaseModel
+
+
+class Product(BaseModel):
+    id: int | None
+    name: str
+    description: str
+    price: float
+    category_id: int
+
+    @classmethod
+    def from_query_result(cls, id, name, description, price, category_id):
+        return cls(
+            id=id,
+            name=name,
+            description=description,
+            price=price,
+            category_id=category_id)
---
+++ main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+# app.include_router(product_router)
+
+# if __name__ == '__main__':
+#     uvicorn.run(app='main:app', host='127.0.0.1', port=8000, reload=True)
