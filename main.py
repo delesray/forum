829ea5c84ad58cc1e23533d1e0680f3cab65d52e
olesya-karpets <olesya.karpec@gmail.com>
2024-04-15 12:04:21 +0300

    topics_router

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import uvicorn
 from fastapi import FastAPI
 from routers.users import users_router
 from routers.categories import categories_router
+from routers.topics import topics_router
 
 app = FastAPI()
 app.include_router(users_router)
 app.include_router(categories_router)
+app.include_router(topics_router)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='127.0.0.1', port=8000)
