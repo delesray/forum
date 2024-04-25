3976b093bf583d0139a374782739c9dee4b5aa44
DenitsaTH <denizza.cristova@gmail.com>
2024-04-25 12:06:41 +0300

    Auth changes

--- common/auth.py
+++ common/auth.py
@@ -1,5 +1,5 @@
 from fastapi import HTTPException, Depends
-from typing import Annotated
+from typing import Annotated, Union
 from data.models import User, TokenData
 from services.users_services import is_authenticated, from_token, find_by_username
 from datetime import timedelta, datetime
@@ -7,9 +7,11 @@ from jose import jwt, JWTError, ExpiredSignatureError
 from data.models import Token, TokenData
 from secret_key import SECRET_KEY
 from fastapi.security import OAuth2PasswordBearer
-from common.responses import Unauthorized, BadRequest
+from common.responses import BadRequest
+
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
 
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
@@ -30,36 +32,39 @@ def is_token_exp_valid(exp: str) -> bool:
     return exp_datetime > datetime.now()
 
 
-def verify_token_access(token: str) -> TokenData | JWTError:
+# Union specifies that the returned type would be either one of these
+def verify_token_access(token: str) -> Union[TokenData, ExpiredSignatureError, JWTError]:
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
         username: str = payload.get("username")
         is_admin: bool = payload.get("is_admin")
         exp_at: str = payload.get("expire")
 
-        # this is needed, don't comment out
         if not is_token_exp_valid(exp_at):
             raise ExpiredSignatureError()
 
         token_data = TokenData(username=username, is_admin=is_admin)
         return token_data
 
-    except ExpiredSignatureError:
-        raise JWTError("Token has expired. Please log in again.")
+    # in case of token exp
+    except ExpiredSignatureError as e:
+        return("Token has expired. Please log in again.")
 
+    # in case of invalid token
     except JWTError as e:
-        return e
+        return "Invalid token"
 
 
-def get_current_user(token: str = Depends(oauth2_scheme)) -> User | BadRequest | Unauthorized:
+def get_current_user(token: str = Depends(oauth2_scheme)) -> Union[User, BadRequest]:
     token_data = verify_token_access(token)
 
+    # will return the correct msg - either invalid token or expired token
     if not isinstance(token_data, TokenData):
-        raise HTTPException(status_code=400, detail="Invalid token")
+        raise HTTPException(status_code=400, detail=token_data)
 
     user = find_by_username(token_data.username)
 
-    # if the token is verified but there is no such user (has been deleted), raise 404
+    # if the token is verified but there is no such user (has been deleted)
     if not user:
         raise HTTPException(status_code=404, detail="No such user")
 
