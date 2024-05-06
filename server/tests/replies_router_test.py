179959e61b57b715779606633d2134d899caef35
DenitsaTH <denizza.cristova@gmail.com>
2024-05-06 23:59:20 +0300

    replies_router_test: add tests

---
+++ server/tests/replies_router_test.py
@@ -0,0 +1,51 @@
+import unittest
+from unittest.mock import Mock, patch
+from routers import replies as replies_router
+from routers import topics as topics_router
+from data.models.reply import ReplyCreateUpdate
+
+mock_replies_service = Mock(autospec=True)
+mock_topics_service = Mock(autospec=True)
+
+replies_router.replies_services = mock_replies_service
+replies_router.topics_services = mock_topics_service
+
+TOPIC_ID = 1
+REPLY_ID = 1
+reply = ReplyCreateUpdate(text='sometext')
+
+
+def fake_user():
+    user = Mock()
+    user.user_id = 1
+
+    return user
+
+
+class RepliesRouter_Should(unittest.TestCase):
+
+    def setUp(self) -> None:
+        mock_replies_service.reset_mock()
+
+    def test_addReply_returnsCorrectMsg_whenUserHasAccessToTopic(self):
+        # arrange
+        mock_topics_service.exists = True
+        mock_replies_service.can_user_access_topic_content = (True, 'OK')
+        mock_replies_service.create_reply.return_value = REPLY_ID
+
+        expected = f'Reply with ID {REPLY_ID} successfully added'
+
+        # act
+        result = replies_router.add_reply(topic_id=TOPIC_ID, reply=reply,
+                                          current_user=fake_user())
+
+        # assert
+        self.assertEqual(expected, result)
+
+
+    def test_raisesHTTPException_statusCode404_whenTopicNotExists(self):
+        pass
+
+
+    def test_raisesHTTPException_statusCode403_whenUsedNotHasAccessToTopic(self):
+        pass
