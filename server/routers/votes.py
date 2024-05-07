ec0b530a3a71e1d7d0ca8dadc3cc740f9cc0b899
DenitsaTH <denizza.cristova@gmail.com>
2024-05-07 19:48:43 +0300

    votes_routers: add str constr for type

--- server/routers/votes.py
+++ server/routers/votes.py
@@ -1,15 +1,21 @@
+from typing import Annotated
 from fastapi import APIRouter, HTTPException
+from pydantic import StringConstraints
 from common.oauth import UserAuthDep
 from common.responses import SC
 from services import votes_services
 from services.replies_services import exists as reply_exists, can_user_access_topic_content
 from services.topics_services import exists as topic_exists
 
-votes_router = APIRouter(prefix='/topics/{topic_id}/replies/{reply_id}/votes', tags=['votes'])
+
+votes_router = APIRouter(
+    prefix='/topics/{topic_id}/replies/{reply_id}/votes', tags=['votes'])
+
+allowed_vote_type = Annotated[str, StringConstraints(pattern=r'^(up|down)$')]
 
 
 @votes_router.get('/', status_code=200)
-def get_all_votes_for_reply(reply_id: int, topic_id: int, type: str, current_user: UserAuthDep):
+def get_all_votes_for_reply(reply_id: int, topic_id: int, type: allowed_vote_type, current_user: UserAuthDep):
     if not topic_exists(id=topic_id):
         raise HTTPException(
             status_code=SC.NotFound,
@@ -22,7 +28,8 @@ def get_all_votes_for_reply(reply_id: int, topic_id: int, type: str, current_use
             detail='No such reply in this topic'
         )
 
-    user_modify_vote, msg = can_user_access_topic_content(topic_id=topic_id, user_id=current_user.user_id)
+    user_modify_vote, msg = can_user_access_topic_content(
+        topic_id=topic_id, user_id=current_user.user_id)
 
     if not user_modify_vote:
         raise HTTPException(
@@ -35,7 +42,8 @@ def get_all_votes_for_reply(reply_id: int, topic_id: int, type: str, current_use
 
 
 @votes_router.put('/', status_code=SC.Created)
-def add_or_switch(type: str, reply_id, topic_id, current_user: UserAuthDep):
+def add_or_switch(type: allowed_vote_type,
+                  reply_id: int, topic_id: int, current_user: UserAuthDep):
     if not topic_exists(id=topic_id):
         raise HTTPException(
             status_code=SC.NotFound,
@@ -48,7 +56,8 @@ def add_or_switch(type: str, reply_id, topic_id, current_user: UserAuthDep):
             detail='No such reply in this topic'
         )
 
-    user_modify_vote, msg = can_user_access_topic_content(topic_id=topic_id, user_id=current_user.user_id)
+    user_modify_vote, msg = can_user_access_topic_content(
+        topic_id=topic_id, user_id=current_user.user_id)
 
     if not user_modify_vote:
         raise HTTPException(
@@ -56,10 +65,12 @@ def add_or_switch(type: str, reply_id, topic_id, current_user: UserAuthDep):
             detail=msg
         )
 
-    vote = votes_services.vote_exists(reply_id=reply_id, user_id=current_user.user_id)
+    vote = votes_services.vote_exists(
+        reply_id=reply_id, user_id=current_user.user_id)
 
     if not vote:
-        votes_services.add_vote(user_id=current_user.user_id, reply_id=reply_id, type=type)
+        votes_services.add_vote(
+            user_id=current_user.user_id, reply_id=reply_id, type=type)
         return f'You {type}voted REPLY with ID: {reply_id}'
 
     votes_services.switch_vote(user_id=current_user.user_id,
@@ -81,7 +92,8 @@ def remove_vote(topic_id: int, reply_id: int, current_user: UserAuthDep):
             detail='No such reply in this topic'
         )
 
-    user_modify_vote, msg = can_user_access_topic_content(topic_id=topic_id, user_id=current_user.user_id)
+    user_modify_vote, msg = can_user_access_topic_content(
+        topic_id=topic_id, user_id=current_user.user_id)
 
     if not user_modify_vote:
         raise HTTPException(
