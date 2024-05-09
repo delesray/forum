06196f90d4a139451ea85f13da20431db5fda36e
miray-mustafov <mustafovmiray@gmail.com>
2024-05-09 17:18:24 +0300

    setup new branch

--- server/main.py
+++ server/main.py
@@ -8,6 +8,10 @@ from routers.replies import replies_router
 from routers.votes import votes_router
 from routers.messages import messages_router
 
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 app = FastAPI()
 app.include_router(users_router)
 app.include_router(categories_router)
@@ -16,6 +20,25 @@ app.include_router(admin_router)
 app.include_router(replies_router)
 app.include_router(votes_router)
 app.include_router(messages_router)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+# todo visit http://127.0.0.1:8000/categories_demo
+@app.get("/categories_demo",
+         response_class=HTMLResponse)  # response_class is for the /docs to know that the response will be HTML
+async def categories_demo_view(
+        request: Request,
+        search: str | None = None
+):
+    from services.categories_services import get_all
+    categories = get_all(search=search)
+
+    return templates.TemplateResponse(
+        request=request, name="categories_demo.html", context={'categories': categories}
+    )
+
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='127.0.0.1', port=8000)
