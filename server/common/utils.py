b71220859c80f32075a8b5bec6d4b41dddfeb3a6
olesya-karpets <olesya.karpec@gmail.com>
2024-05-11 08:48:52 +0300

    utils.py: updated result_url()

--- server/common/utils.py
+++ server/common/utils.py
@@ -29,7 +29,7 @@ class PaginationInfo(BaseModel):
 
 
 class Links(BaseModel):
-    self: str
+    current_url: str
     first: str
     last: str
     next: str | None
@@ -47,12 +47,21 @@ def get_pagination_info(total_elements, page, size) -> PaginationInfo:
 
 
 def create_links(request: Request, pagination_info: PaginationInfo) -> Links:
-    # base_url = str(request.url_for("get_all_topics"))
+    """
+    Creates pagination links based on the provided request and pagination information.
+
+    Parameters:
+        request (Request): HTTP request object containing URL information.
+        pagination_info (PaginationInfo): Information about pagination, such as total elements, current page, and page size.
+
+    Returns:
+        Links: An object containing pagination links, including current, first, last, next, and previous links.
+    """
+   
     pi = pagination_info
 
-    # todo discuss: because self is keyword?
     links = Links(
-        self=f"{request.url}",
+        current_url=f"{request.url}",
         first=f"{result_url(request, 1, pi.size)}",
         last=f"{result_url(request, pi.pages, pi.size)}",
         next=f"{result_url(request, pi.page + 1, pi.size)}" if pi.page * pi.size < pi.total_elements else None,
@@ -62,13 +71,25 @@ def create_links(request: Request, pagination_info: PaginationInfo) -> Links:
 
 
 def result_url(request: Request, page: int, size: int) -> str:
+    """
+    Constructs a new URL based on the provided request URL with updated query parameters for pagination.
+
+    Parameters:
+        request (Request): HTTP request object containing the URL information.
+        page (int): page number for pagination.
+        size (int): page size for pagination.
+
+    Returns:
+        str: The constructed URL with updated query parameters for pagination.
+    """
     parsed_query = parse_qs(request.url.query)
 
     parsed_query.update({'page': [str(page)], 'size': [str(size)]})
     new_query = '&'.join(f'{key}={val[0]}' for key, val in parsed_query.items())
-
+    
+    #constructs the base URL combining the scheme, netloc(includes hostname and port num),and path components from the request.url
     new_url = f'{request.url.scheme}://{request.url.netloc}{request.url.path}'
-    if new_query:
-        new_url += f'?{new_query}'
+  
+    new_url += f'?{new_query}'
 
     return new_url
