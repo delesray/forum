994d8475e2ece7880396abc358f50b8ac5aec3b5
miray-mustafov <mustafovmiray@gmail.com>
2024-05-13 10:05:28 +0300

    forum_db_seed: update

--- server/data/forum_db_seed.sql
+++ server/data/forum_db_seed.sql
@@ -39,7 +39,7 @@ INSERT INTO topics(title,user_id,category_id) VALUES('Where to go fishing?', 2,6
 
 -- INSERTING PERMISSIONS
 INSERT INTO users_categories_permissions(user_id,category_id) VALUES(3,6); -- readonly
-INSERT INTO users_categories_permissions(user_id,category_id,wcrite_access) VALUES(4, 6, 1);-- write
+INSERT INTO users_categories_permissions(user_id,category_id,write_access) VALUES(4, 6, 1);-- write
 
 -- INSERTING REPLIES
 INSERT INTO replies(text,user_id,topic_id) VALUES('By the river',4, 10); -- 1
